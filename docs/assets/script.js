1007db71059a419247d871031033abe00e9c25c1
Victor <victorze@protonmail.com>
2022-09-14 23:57:47 -0500

    Remove ejs and refactor

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,18 +1,18 @@
 if (window.isCMS) {
   document.addEventListener('DOMContentLoaded', function () {
-    var regions = document.querySelectorAll('.cms-editable');
-    var i;
+    var regions = document.querySelectorAll('.cms-editable')
+    var i
     for (i = 0; i < regions.length; i++) {
       regions[i].addEventListener('ready', function () {
-        Prism.highlightAllUnder(regions[i]);
-      });
+        Prism.highlightAllUnder(regions[i])
+      })
       regions[i].addEventListener('render', function (event) {
         if (event.target.classList.contains('cms-code')) {
-          Prism.highlightAllUnder(regions[i]);
+          Prism.highlightAllUnder(regions[i])
         }
-      });
+      })
     }
-  });
+  })
 } else {
-  Prism.highlightAll();
+  Prism.highlightAll()
 }
--- script.js
+++ script.js
@@ -1,75 +1,76 @@
-const fs = require("fs");
-const path = require("path");
-const matter = require("gray-matter");
-const pug = require('pug');
-const { Remarkable } = require('remarkable');
+const fs = require('fs')
+const path = require('path')
+const matter = require('gray-matter')
+const pug = require('pug')
+const { Remarkable } = require('remarkable')
 
 const postsDirectory = path.join(process.cwd(), 'posts')
+
+renderHome()
+renderPosts()
 
 function renderPosts() {
   getAllPostIds().forEach(id => {
     getPostData(id)
       .then(data => {
         if (data.published) {
-          const layoutPath = path.join(process.cwd(), "layouts", "post.pug");
+          const layoutPath = path.join(process.cwd(), 'layouts', 'post.pug')
           const str = pug.renderFile(layoutPath, {...data, formatDate})
-          const renderPath = path.join(process.cwd(), "docs", "posts", `${id}.html`);
-          fs.writeFileSync(renderPath, str);
+          const renderPath = path.join(process.cwd(), 'docs', 'posts', `${id}.html`)
+          fs.writeFileSync(renderPath, str)
         }
-      });
-  });
+      })
+  })
 }
-renderPosts();
 
 function renderHome() {
-  let posts = getSortedPostsData();
-  posts = posts.filter(post => post.published);
-  const layoutPath = path.join(process.cwd(), "layouts", "index.pug");
+  let posts = getSortedPostsData()
+  posts = posts.filter(post => post.published)
+  const layoutPath = path.join(process.cwd(), 'layouts', 'index.pug')
   const str = pug.renderFile(layoutPath, {posts, formatDate})
-  const renderPath = path.join(process.cwd(), "docs", "index.html");
-  fs.writeFileSync(renderPath, str);
+  const renderPath = path.join(process.cwd(), 'docs', 'index.html')
+  fs.writeFileSync(renderPath, str)
 }
-renderHome();
 
 function formatDate(isoDate) {
-  const [year, month, day] = isoDate.split("-");
-  const monthNames = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio",
-                      "agosto", "setiembre", "octubre", "noviembre", "diciembre"];
-  const monthName = monthNames[Number.parseInt(month) - 1];
-  return `${Number.parseInt(day)} de ${monthName} de ${year}`;
+  const [year, month, day] = isoDate.split('-')
+  const monthNames = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio',
+                      'agosto', 'setiembre', 'octubre', 'noviembre', 'diciembre']
+  const monthName = monthNames[Number.parseInt(month) - 1]
+  return `${Number.parseInt(day)} de ${monthName} de ${year}`
 }
 
 function getSortedPostsData() {
-  const fileNames = fs.readdirSync(postsDirectory);
+  const fileNames = fs.readdirSync(postsDirectory)
   const allPostsData = fileNames.map(fileName => {
-    const id = fileName.replace(/\.md$/, '');
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-    const matterResult = matter(fileContents);
+    const id = fileName.replace(/\.md$/, '')
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const matterResult = matter(fileContents)
     return {
       id,
       ...matterResult.data
     }
-  });
+  })
 
   return allPostsData.sort((a, b) => (a.date < b.date) ? 1 : -1)
-          .filter(post => post.published);
+          .filter(post => post.published)
 }
 
 function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory);
-  return fileNames.map(fileName => fileName.replace(/\.md$/, ''));
+  const fileNames = fs.readdirSync(postsDirectory)
+  return fileNames.map(fileName => fileName.replace(/\.md$/, ''))
 }
 
 async function getPostData(id) {
-  const md = new Remarkable();
-  const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
-  const matterResult = matter(fileContents);
+  const md = new Remarkable()
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const matterResult = matter(fileContents)
   const contentHtml = md.render(matterResult.content)
   return {
     id,
     contentHtml,
     ...matterResult.data
-  };
+  }
 }
